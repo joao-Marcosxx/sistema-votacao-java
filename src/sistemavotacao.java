b4bf0a749766cadb90d306a9c84e82d69789a6f2
Joao Marcos <Joao.111871946@aluno.mg.gov.br>
2026-08-27 13:40:00 -0300

    chore: criar estrutura inicial do programa

---
+++ src/sistemavotacao.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class sistemavotacao {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Sistema de votação iniciado.");
+        scanner.close();
+    }
+}
