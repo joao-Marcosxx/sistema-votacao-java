62db170ec659b9df4a9dbd6d6ff8ef581c676ab6
Joao Marcos <Joao.111871946@aluno.mg.gov.br>
2026-08-27 13:41:59 -0300

    feat: definir limites de candidatos turmas e votantes

--- src/sistemavotacao.java
+++ src/sistemavotacao.java
@@ -7,3 +7,10 @@ public class sistemavotacao {
         scanner.close();
     }
 }
+
+  static final int MAX_CANDIDATOS = 5;
+  static final int TOTAL_TURMAS = 3;
+  static final int MAX_VOTANTES_POR_TURMA = 10;
+
+
+
