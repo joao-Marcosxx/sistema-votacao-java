61a8c7cfc4cdca17e3384d5381d22abe4193f033
Joao Marcos <Joao.111871946@aluno.mg.gov.br>
2026-08-28 15:53:06 -0300

    feat: concluir cadastro de candidatos

--- src/sistemavotacao.java
+++ src/sistemavotacao.java
@@ -133,6 +133,35 @@ static void cadastrarCandidatos(int maxCandidatos) {
 }
 
 
+static void cadastrarCandidatos() {
+
+
+    int quantidade = 0;
+    for (int i = 0; i < quantidade; i++) {
+        int numero = 0;
+        String nome;
+
+        do {
+            System.out.print("Nome do candidato: ");
+            nome = sistemavotacao.scanner.nextLine().trim();
+
+            if (nome.isEmpty()) {
+                System.out.println("O nome não pode ficar vazio.");
+            }
+        } while (nome.isEmpty());
+
+
+        numerosCandidatos[i] = numero;
+        nomesCandidatos[i] = nome;
+        votosCandidatos[i] = 0;
+        quantidadeCandidatos++;
+    }
+}
+
+
+
+
+
 
 
 
